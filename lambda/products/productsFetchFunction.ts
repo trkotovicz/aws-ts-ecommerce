9143d8d0f09873e6e5d6784411e9329eea6802b7
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-02-27 22:31:35 -0300

    feat: gera logs na função Lambda

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,8 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-
+  const lambdaRequestId = context.awsRequestId;
+  const apiRequestId = event.requestContext.requestId;
   const method = event.httpMethod;
+
+  console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`);
 
   if (event.resource === '/products') {
     if (method === 'GET') {
