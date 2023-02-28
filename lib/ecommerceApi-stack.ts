22f8beb63397fa4b9f3b01d4cd6af13561b7db5f
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-02-27 23:05:15 -0300

    feat: gera logs no API Gateway

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -13,8 +13,23 @@ export class ECommerceApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: ECommerceApiStackProps) {
     super(scope, id, props)
   
+    const logGroup = new cwlogs.LogGroup(this, 'ECommerceApiLogs');
     const api = new apigateway.RestApi(this, 'ECommerceApi', {
-      restApiName: 'ECommerceApi'
+      restApiName: 'ECommerceApi',
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true
+        })
+      }
     });
 
     const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler);
